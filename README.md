8e2ba1cbac8b6bf6686470fba4a7e947eb6d17d7
leviosacz <c.expecto.patronum@gmail.com>
2023-02-07 21:58:16 -0800

    fixed pthread bug, work fine

---
+++ README.md
@@ -0,0 +1,53 @@
+# HTTP Web Server
+
+
+
+## Overview
+
+This is a simple web server application using C. The server supports the http 1.0 protocol. It is capable of supporting HTML, TXT, JPG, GIF, JavaScript, CSS, and PNG files. It supports the 200, 404, 403 and 400 HTTP status codes. In addition to status codes, it also supports the Content-Type, Content-Length, and Date headers in responses.
+
+The program runs on an infinite loop where the server accepts client connections from a user supplied port. For every connection it accepts, it creates a thread to handle the request, i.e. multithreading. In the request thread, the server parses the http request, and logs it. It checks if the request is well formed and sends an error otherwise. It then checks if the file exists and if permissions are set properly, and returns an error otherwise. Afterwards, it transmit the file and closes the connection.
+
+
+
+## File List
+
+- httpServer.c
+
+  The c source code for this application.
+
+- Makefile
+
+  Makefile for this project.
+
+- document_root
+
+  a folder containing html, css, js and jpg files. It contains the necessary html files to load the www.scu.edu homepage and handle errors.
+
+  
+
+## How to Run the Program
+
+1. Run the command `make` in the terminal to create the executable “httpServer”.
+
+2. Type the command
+
+   `./httpServer -document_root document_root -port 8888`
+
+   8888 can be replaced with any valid port number. Note that ports between 8000 and 9999 are recommended for testing purposes.
+
+
+
+## Screenshots
+
+- Create the executable "httpServer" and run the server.
+
+  
+
+- A web browser accessing the web server through localhost:8888.
+
+  
+
+- The web server accepting the user's request.
+
+  
--- httpServer.c
+++ httpServer.c
@@ -12,288 +12,253 @@
 #define MAX 8000
 #define BUFFER_SIZE 8000
 
-struct args
-{
-	char *document_root;
+struct args {
+	const char *document_root;
 	int connfd;
 };
 
-void serveClient(int connfd, const char *fileLocation)
-{
+void serveClient(int connfd, const char *fileLocation){
 
-	FILE *filePtr = fopen(fileLocation, "r");
-	if (NULL == filePtr)
-	{
+	FILE *filePtr = fopen(fileLocation,"r");
+	if (NULL == filePtr){
 		perror("Failed: ");
 		return;
 	}
 	int fd = fileno(filePtr);
-	printf("server succesfully opened requested file:%d\n", fd);
+	printf("server succesfully opened requested file:%d\n",fd);
 
 	struct stat st;
-	fstat(fd, &st);
+	fstat(fd,&st);
 	off_t file_size = st.st_size;
 
 	char file_buff[BUFFER_SIZE];
 
 	int n;
-	do
-	{
+	do{
 		bzero(file_buff, BUFFER_SIZE);
-		n = fread(file_buff, 1, BUFFER_SIZE, filePtr);
-		if (n > 0)
-		{
+		n = fread(file_buff,1,BUFFER_SIZE,filePtr);
+		if (n>0){
 			write(connfd, file_buff, n);
 		}
-	} while (n > 0);
+	}while(n>0);
 	fclose(filePtr);
 }
 
-void *handleRequestThread(void *thread_args)
-{
+void *handleRequestThread(void *thread_args){
 
-	// parsing supplied arguments
+	//parsing supplied arguments
 	struct args *passed_args;
-	passed_args = (struct args *)thread_args;
-	char *document_root = passed_args->document_root;
+	passed_args = (struct args*) thread_args;
+	const char *document_root = passed_args->document_root;
 	int connfd = passed_args->connfd;
 
-	// first parse the http header sent by the client
+
+	//first parse the http header sent by the client
 	char requestBuffer[BUFFER_SIZE];
-	bzero(requestBuffer, BUFFER_SIZE);
+	bzero(requestBuffer,BUFFER_SIZE);
 	read(connfd, requestBuffer, sizeof(requestBuffer));
 
-	// Log the client request on the server's console log
+	//Log the client request on the server's console log
 	printf("Server recieved client request\n");
 	printf("Client Request Header:\n");
-	printf("%s\n", requestBuffer);
+	printf("%s\n",requestBuffer);
 
-	// get the time from the server
+	//get the time from the server
 	char timestr[200];
 	time_t now = time(0);
 	struct tm tm = *gmtime(&now);
 	strftime(timestr, 200, "%a, %d %b %Y %H:%M:%S %Z", &tm);
 	printf("Server time: %s\n", timestr);
 
-	char *httpMethod = strtok(requestBuffer, " ");
+	char *httpMethod = strtok(requestBuffer, " ");	
 	char *requestedFile = strtok(NULL, " ");
 
-	// check for any bad requests
-	if (strncmp(requestedFile, "//", 2) == 0)
-	{
+	//check for any bad requests
+	if(strncmp(requestedFile,"//",2)==0){
 		char responseHeader[1000] = "HTTP/1.0 400 Bad Request\nContent-Type: text/html;charset=UTF-8\nContent-Length: 100000000\r\n";
 
-		strcat(responseHeader, "Date: ");
-		strcat(responseHeader, timestr);
-		strcat(responseHeader, "\r\n\r\n");
-		// strcat(responseHeader,"Date: ");
-		// strcat(responseHeader,timestr);
-		// strcat(responseHeader,"\n\n");
+		strcat(responseHeader,"Date: ");
+		strcat(responseHeader,timestr);
+		strcat(responseHeader,"\r\n\r\n");
 		write(connfd, responseHeader, strlen(responseHeader));
 		char formattedFile[BUFFER_SIZE];
-		strcat(formattedFile, document_root);
-		strcat(formattedFile, "/http400.html");
-		serveClient(connfd, formattedFile);
+		strcat(formattedFile,document_root);
+		strcat(formattedFile,"/http400.html");
+		serveClient(connfd,formattedFile);
 		close(connfd);
 		pthread_exit(NULL);
 		return NULL;
+
 	}
 
-	// first handle the default / loading page
-	if (strcmp(requestedFile, "/") == 0)
-	{
+	//first handle the default / loading page
+	if(strcmp(requestedFile,"/")==0){
 		char responseHeader[1000] = "HTTP/1.0 200 OK\nContent-Type: text/html;charset=UTF-8\nContent-Length: 100000000\r\n";
 
-		strcat(responseHeader, "Date: ");
-		strcat(responseHeader, timestr);
-		strcat(responseHeader, "\r\n\r\n");
-		// strcat(responseHeader,"Date: ");
-		// strcat(responseHeader,timestr);
-		// strcat(responseHeader,"\n\n");
+		strcat(responseHeader,"Date: ");
+		strcat(responseHeader,timestr);
+		strcat(responseHeader,"\r\n\r\n");
 		write(connfd, responseHeader, strlen(responseHeader));
 		char formattedFile[BUFFER_SIZE];
-		strcat(formattedFile, document_root);
-		strcat(formattedFile, "/index.html");
-		serveClient(connfd, formattedFile);
+		strcat(formattedFile,document_root);
+		strcat(formattedFile,"/index.html");
+		serveClient(connfd,formattedFile);
 		close(connfd);
 		pthread_exit(NULL);
 		return NULL;
 	}
 
-	// check the requested file
+	//check the requested file
 	char formattedFile[BUFFER_SIZE];
-	strcat(formattedFile, document_root);
-	strcat(formattedFile, requestedFile);
+	strcat(formattedFile,document_root);
+	strcat(formattedFile,requestedFile);
 	printf("requested file is: %s\n", formattedFile);
 	char statusCode[40];
 	FILE *filePtr;
-	filePtr = fopen(formattedFile, "r");
-	if (NULL == filePtr)
-	{
+	filePtr = fopen(formattedFile,"r");
+	if (NULL == filePtr){
 		perror("unable to open file\n");
-		if (errno == 2)
-		{
+		if (errno == 2){
 			strcpy(statusCode, "404 Not Found");
 		}
-		else if (errno == 13)
-		{
+		else if(errno == 13){
 			strcpy(statusCode, "403 Forbidden");
 		}
 	}
-	else
-	{
+	else{
 		strcpy(statusCode, "200 OK");
 	}
-
+	
 	char responseHeader[1000] = "HTTP/1.0 ";
 	strcat(responseHeader, statusCode);
 	strcat(responseHeader, "\nContent-Type: ");
 	char *contentLength = "\nContent-Length: 100000000\r\n";
 
-	// handle error codes 404 and 403
-	if (strcmp(statusCode, "404 Not Found") == 0)
-	{
+	//handle error codes 404 and 403
+	if(strcmp(statusCode,"404 Not Found")==0){
 		printf("Server Error, file requested resulted in: %s\n", statusCode);
 		char responseHeader[1000] = "HTTP/1.0 404 Not Found\nContent-Type: text/html;charset=UTF-8\nContent-Length: 100000000\r\n";
 
-		strcat(responseHeader, "Date: ");
-		strcat(responseHeader, timestr);
-		strcat(responseHeader, "\r\n\r\n");
-		// strcat(responseHeader,"Date: ");
-		// strcat(responseHeader,timestr);
-		// strcat(responseHeader,"\n\n");
+		strcat(responseHeader,"Date: ");
+		strcat(responseHeader,timestr);
+		strcat(responseHeader,"\r\n\r\n");
 		write(connfd, responseHeader, strlen(responseHeader));
 		char formattedFile[BUFFER_SIZE];
-		strcat(formattedFile, document_root);
-		strcat(formattedFile, "/http404.html");
-		serveClient(connfd, formattedFile);
+		strcat(formattedFile,document_root);
+		strcat(formattedFile,"/http404.html");
+		serveClient(connfd,formattedFile);
 		close(connfd);
 		pthread_exit(NULL);
 		return NULL;
-	}
-	else if (strcmp(statusCode, "403 Forbidden") == 0)
-	{
+
+
+	}else if(strcmp(statusCode,"403 Forbidden")==0){
 		printf("Server Error, file requested resulted in: %s\n", statusCode);
 		char responseHeader[1000] = "HTTP/1.0 403 Forbidden\nContent-Type: text/html;charset=UTF-8\nContent-Length: 100000000\r\n";
 
-		strcat(responseHeader, "Date: ");
-		strcat(responseHeader, timestr);
-		strcat(responseHeader, "\r\n\r\n");
-		// strcat(responseHeader,"Date: ");
-		// strcat(responseHeader,timestr);
-		// strcat(responseHeader,"\n\n");
+		strcat(responseHeader,"Date: ");
+		strcat(responseHeader,timestr);
+		strcat(responseHeader,"\r\n\r\n");
 		write(connfd, responseHeader, strlen(responseHeader));
 		char formattedFile[BUFFER_SIZE];
-		strcat(formattedFile, document_root);
-		strcat(formattedFile, "/http403.html");
-		serveClient(connfd, formattedFile);
+		strcat(formattedFile,document_root);
+		strcat(formattedFile,"/http403.html");
+		serveClient(connfd,formattedFile);
 		close(connfd);
 		pthread_exit(NULL);
 		return NULL;
+
 	}
 
-	// content type parsing
-	char *contentType = strrchr(requestedFile, '.');
+	//content type parsing
+	char *contentType = strrchr(requestedFile,'.');
 	printf("content type detected is: %s\n", contentType);
-	if (NULL == contentType)
-	{
+	if (NULL == contentType){
 		printf("file is not supported\n");
-		// invalid file type, sending 400
+		//invalid file type, sending 400
 		char responseHeader[1000] = "HTTP/1.0 400 Bad Request\nContent-Type: text/html;charset=UTF-8\nContent-Length: 100000000\r\n";
-
-		strcat(responseHeader, "Date: ");
-		strcat(responseHeader, timestr);
-		strcat(responseHeader, "\r\n\r\n");
-		// strcat(responseHeader,"Date: ");
-		// strcat(responseHeader,timestr);
-		// strcat(responseHeader,"\n\n");
+	
+		strcat(responseHeader,"Date: ");
+		strcat(responseHeader,timestr);
+		strcat(responseHeader,"\r\n\r\n");
 		write(connfd, responseHeader, strlen(responseHeader));
 		char formattedFile[BUFFER_SIZE];
-		strcat(formattedFile, document_root);
-		strcat(formattedFile, "/http400.html");
-		serveClient(connfd, formattedFile);
+		strcat(formattedFile,document_root);
+		strcat(formattedFile,"/http400.html");
+		serveClient(connfd,formattedFile);
 		close(connfd);
 		pthread_exit(NULL);
 		return NULL;
+
+		
 	}
-	if (strncmp(contentType, ".html", 5) == 0)
-	{
+	if (strncmp(contentType, ".html", 5)==0){
 		strcat(responseHeader, "text/html;charset=UTF-8");
 	}
-	else if (strncmp(contentType, ".txt", 4) == 0)
-	{
+	else if (strncmp(contentType, ".txt", 4)==0){
 		strcat(responseHeader, "text/txt");
 	}
-	else if (strncmp(contentType, ".js", 3) == 0)
-	{
+	else if (strncmp(contentType, ".js", 3)==0){
 		strcat(responseHeader, "text/js");
 	}
-	else if (strncmp(contentType, ".css", 4) == 0)
-	{
+	else if (strncmp(contentType, ".css", 4)==0){
 		strcat(responseHeader, "text/css");
 	}
-	else if ((strncmp(contentType, ".jpg", 4) == 0) || (strncmp(contentType, ".ico", 4) == 0))
-	{
+	else if((strncmp(contentType, ".jpg", 4)==0) || (strncmp(contentType, ".ico", 4)==0)){
 		strcat(responseHeader, "image/jpg");
 	}
-	else if (strncmp(contentType, ".gif", 4) == 0)
-	{
+	else if (strncmp(contentType, ".gif", 4)==0){
 		strcat(responseHeader, "image/gif");
 	}
-	else if (strncmp(contentType, ".png", 4) == 0)
-	{
+	else if (strncmp(contentType, ".png", 4)==0){
 		strcat(responseHeader, "image/png");
-	}
-	else
-	{
-		// invalid file type, sending 400
+	}else{
+		//invalid file type, sending 400
 		char responseHeader[1000] = "HTTP/1.0 400 Bad Request\nContent-Type: text/html;charset=UTF-8\nContent-Length: 100000000\r\n";
 
-		strcat(responseHeader, "Date: ");
-		strcat(responseHeader, timestr);
-		strcat(responseHeader, "\r\n\r\n");
+		strcat(responseHeader,"Date: ");
+		strcat(responseHeader,timestr);
+		strcat(responseHeader,"\r\n\r\n");
 		write(connfd, responseHeader, strlen(responseHeader));
 		char formattedFile[BUFFER_SIZE];
-		strcat(formattedFile, document_root);
-		strcat(formattedFile, "/http400.html");
-		serveClient(connfd, formattedFile);
+		strcat(formattedFile,document_root);
+		strcat(formattedFile,"/http400.html");
+		serveClient(connfd,formattedFile);
 		close(connfd);
 		pthread_exit(NULL);
 		return NULL;
 	}
-
-	// content length addition
-	strcat(responseHeader, "\nContent-Length: 100000000\r\n");
-	strcat(responseHeader, "Date: ");
-	strcat(responseHeader, timestr);
-	strcat(responseHeader, "\r\n\r\n");
+	
+	//content length addition
+	strcat(responseHeader,"\nContent-Length: 100000000\r\n"); 
+	strcat(responseHeader,"Date: ");
+	strcat(responseHeader,timestr);
+	strcat(responseHeader,"\r\n\r\n");
 	write(connfd, responseHeader, strlen(responseHeader));
 
-	// serve the file
-	serveClient(connfd, formattedFile);
+	//serve the file
+	serveClient(connfd,formattedFile);
 	close(connfd);
 	pthread_exit(NULL);
 
 	return NULL;
 }
 
-int startServerAtPort(int port)
-{
+int startServerAtPort(int port){
 	int sockfd;
 
-	// open a tcp socket
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
+	//open a tcp socket
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0))<0){
 		perror("unable to open a tcp socket\n");
 		exit(1);
 	}
 	printf("server tcp socket successfully opened\n");
 
-	// allow for immediate use of the port
+	//allow for immediate use of the port
 	int sockoptval = 1;
 	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &sockoptval, sizeof(int));
-
-	// bind the tcp socket to a port
+	
+	//bind the tcp socket to a port
 	struct sockaddr_in serverAddr;
 	memset((char *)&serverAddr, 0, sizeof(serverAddr));
 
@@ -301,65 +266,62 @@ int startServerAtPort(int port)
 	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serverAddr.sin_port = htons(port);
 
-	if (bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
-	{
-		perror("binding of tcp socket failed\n");
+	if (bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr))<0){
+		perror("binding of tcp socket failed\n");	
 		exit(1);
 	}
-	printf("server tcp socket sucessfully binded to port: %d\n", port);
+	printf("server tcp socket sucessfully binded to port: %d\n",port);
 
-	// accept the connection with a queue backlog of 50
-	if (listen(sockfd, 50) < 0)
-	{
+	//accept the connection with a queue backlog of 50
+	if (listen(sockfd,50)<0){
 		perror("listening failed\n");
 		exit(1);
 	}
-	printf("server now listening to tcp socket %d\n", sockfd);
+	printf("server now listening to tcp socket %d\n",sockfd);
 
 	return sockfd;
+
 }
 
-int main(int argc, char *argv[])
-{
 
-	if (argc != 5)
-	{
+int main(int argc, char *argv[]){
+
+	if (argc != 5){
 		printf("incorrect number of arguments given\n");
 		printf("number of arguments given: %d\n", argc);
 		printf("failed to start the server\n");
-		return -1;
+		return -1;	
 	}
 
-	int port = atoi(argv[4]);
-	char *document_root = argv[2];
+	int port = atoi(argv[4]);	
+	const char *document_root = argv[2];
 
 	int sockfd = startServerAtPort(port);
-
-	// client connection information
+	
+	//client connection information
 	int connfd;
 	struct sockaddr_in clientAddr;
 	socklen_t alen;
+	
+	while(1){
 
-	while (1)
-	{
-
-		if ((connfd = accept(sockfd, (struct sockaddr *)&clientAddr, &alen)) < 0)
-		{
+		if ((connfd = accept(sockfd, (struct sockaddr *)&clientAddr, &alen))<0){
 			perror("unable to accept client\n");
 			exit(1);
 		}
-		printf("Server accepted client %d\n", connfd);
+		printf("Server accepted client %d\n",connfd);
 
-		struct args *thread_args = (struct args *)malloc(sizeof(struct args));
-		thread_args->document_root = document_root;
+		struct args * thread_args = (struct args *)malloc(sizeof(struct args));
+		thread_args->document_root = (char *)document_root;
 		thread_args->connfd = connfd;
 
 		pthread_t t;
 		int *pclient = malloc(sizeof(int));
 		*pclient = connfd;
 
-		pthread_create(&t, NULL, handleRequestThread, (void *)thread_args);
+		pthread_create(&t, NULL,handleRequestThread,(void *)thread_args); 
 	}
 
 	return 0;
+	
 }
